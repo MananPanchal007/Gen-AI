03863afbca567a4321e215ebc71a1e3a8c880f8f
MananPanchal007 <mananmp3232@gmail.com>
2025-05-17 06:37:05 -0400

    a

--- Tts and Stt/main.py
+++ Tts and Stt/main.py
@@ -2,8 +2,6 @@ from dotenv import load_dotenv
 import speech_recognition as sr
 from langgraph.checkpoint.mongodb import MongoDBSaver
 from graph import create_chat_graph
-import asyncio
-from openai.helpers import LocalAudioPlayer
 
 from openai import AsyncOpenAI
 
@@ -13,13 +11,12 @@ openai = AsyncOpenAI()
 
 MONGODB_URI = "mongodb://admin:admin@localhost:27017"
 config = {"configurable": {"thread_id": "7"}}
- 
+
 
 def main():
     with MongoDBSaver.from_conn_string(MONGODB_URI) as checkpointer:
         graph = create_chat_graph(checkpointer=checkpointer)
-        
-        
+
         r = sr.Recognizer()
 
         with sr.Microphone() as source:
@@ -34,22 +31,9 @@ def main():
                 sst = r.recognize_google(audio)
 
                 print("You Said:", sst)
-                for event in graph.stream({ "messages": [{"role": "user", "content": sst}] }, config, stream_mode="values"):
+                for event in graph.stream({"messages": [{"role": "user", "content": sst}]}, config, stream_mode="values"):
                     if "messages" in event:
-                            event["messages"][-1].pretty_print()
+                        event["messages"][-1].pretty_print()
 
-
-async def speak(text: str):
-    async with openai.audio.speech.with_streaming_response.create(
-        model="gpt-4o-mini-tts",
-        voice="coral",
-        input=text,
-        instructions="Speak in a cheerful and positive tone.",
-        response_format="pcm",
-    ) as response:
-        await LocalAudioPlayer().play(response)
 
 main()
-
-# if __name__ == "__main__":
-#      asyncio.run(speak(text="This is a sample voice. Hi Piyush"))
